c9835f2f918b44c79fe6ab8c3621e6653753d398
stolenwhale <44807919+stolenwhale@users.noreply.github.com>
2019-10-29 14:17:50 +0300

    Добавлена версия с использованием Google API

--- app.js
+++ app.js
@@ -1,63 +1,61 @@
-const Excel = require("exceljs");
-const rp = require("request-promise");
+const Excel = require("exceljs"),
+      rp = require("request-promise");
 
-const myKey = 'ВВЕДИТЕ ВАШ КЛЮЧ';
-
-console.log("Я родился и начал работу");
+console.log("Я родился и начал работу. Подождите, пока я закончу");
+const myKey = 'ВВЕДИТЕ ВАШ КЛЮЧ'
 
 // инициализация excel-файла
 const workbook = new Excel.Workbook();
 workbook.xlsx.readFile("Адресная программа.xlsx").then(function() {
   let addresses = [];
-  const worksheet = workbook.getWorksheet("Адресная программа");
+  const worksheet = workbook.getWorksheet("Координаты");
 
-  // считываем все адреса из первого столбца excel-файла
+  // считываем все адреса из первого столбца
   const columnToRead = worksheet.getColumn(1);
   const totalRowQuantity = columnToRead.values.length - 1;
   for (let i = 2; i <= totalRowQuantity; i++) {
     addresses.push(columnToRead.values[i]);
   }
 
-  // делаем запрос на сервер с задержкой с помощью бесплатного API для геокодинга: https://locationiq.com
-    let i = 0,
-    lat = [],
-    lng = [];
+  // делаем запрос на сервер с задержкой:
+  let i = 0,
+    coords = [];
 
-  async function sendRequest() {
-    const url = "https://eu1.locationiq.com/v1/search.php";
+  (async function sendRequest() {
+    const url = "https://maps.googleapis.com/maps/api/geocode/json";
     const options = {
       method: "GET",
       url: url,
       qs: {
-        key: myKey, 
-        q: addresses[i],
-        format: "json",
-        limit: 1
+        address: 'Санкт-Петербург ' + addresses[i],
+        key: myKey
       },
       json: true
     };
 
     rp(options).then(function(response) {
-      lat.push(response[0].lat);
-      lng.push(response[0].lon);
+      coords.push(response["results"][0]["geometry"]["location"]);
 
-      // записываем результаты в excel-файл
-      const columnToWrite = worksheet.getColumn(2);
-      for (let count = 0; count < addresses.length; count++) {
-        worksheet.getRow(count + 2).getCell(2).value = lat[count];
-        worksheet.getRow(count + 2).getCell(3).value = lng[count];
+      console.log(".");
+      i++;
+      if (i < addresses.length) {
+        setTimeout(sendRequest, 100);
+      } else {
+        // после получения ответа по всем адресам записываем результаты в Excel:
+        writeToExcel();  
       }
-      return workbook.xlsx.writeFile("Адресная программа.xlsx");
     });
-
-    console.log(".");
-    
-    i++;
-    if (i < addresses.length) {
-
-      // в бесплатной версии API разрешены 2 запроса в секунду
-      setTimeout(sendRequest, 600);
+  })();
+ 
+ 
+  function writeToExcel() {
+    console.log("Сохраняю");
+    worksheet.getColumn(2);
+    for (let count = 0; count < addresses.length; count++) {
+      worksheet.getRow(count + 2).getCell(2).value = coords[count]["lat"];
+      worksheet.getRow(count + 2).getCell(3).value = coords[count]["lng"];
     }
+    return workbook.xlsx.writeFile("Адресная программа.xlsx");
   }
-  sendRequest();
 });
+
